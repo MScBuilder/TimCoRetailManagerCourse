f88338c6cde316611ccf9e2bb8c6321847e6a1ad
Dariusz Lyssy <darek.lyssy@gmail.com>
2019-03-15 22:12:52 +0100

    Wire up Login Form to Api
    Connectis the login form button to the authentication API endpoint {/Token}. Gets back the barrer token or exception if it failed.

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -33,7 +33,9 @@ namespace TRMDesktopUI
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
+
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
